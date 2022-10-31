c98b82683e26d827aa05472ef0b4217a7b1618c7
Lucas Guimarães <lucasjurema11@gmail.com>
2022-10-31 19:30:13 -0300

    Finalizado funcional

--- tecnicas_de_programacao_I/projetos/calculadora/calculadora/__init__.py
+++ tecnicas_de_programacao_I/projetos/calculadora/calculadora/__init__.py
@@ -1,6 +1,17 @@
-from funcoes import soma, subtração, multiplicao, divisao
+from funcoes import soma, subtração, multiplicacao, divisao
 
 
-num01 = input('Digite um numero:')
-num02 = input('Digite um 2 numero:')
-operacao = input('Insira a operação 'soma', 'subtracao', 'divisao', 'multiplicacao', '+', '-', '/', '*')
+num01 = float(input('Digite um numero:'))
+num02 = float(input('Digite um 2 numero:'))
+operacao = input("Insira a operação 'soma', 'subtracao', 'divisao', 'multiplicacao', '+', '-', '/', '*'")
+
+if operacao == 'soma' or operacao == '+':
+    resultado = soma(num01, num02)
+elif operacao == 'subtracao' or operacao == '-':
+    resultado = subtração(num01, num02)
+elif operacao == 'multiplicacao' or operacao == '*':
+    resultado = multiplicacao(num01, num02)
+elif operacao == 'divisao' or operacao == '/':
+    resultado = divisao(num01, num02)
+
+print(f'O resultado da operação é {resultado}')
